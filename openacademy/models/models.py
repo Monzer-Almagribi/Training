ebf35535091890927af0d3d68bf71c6867da3b11
monzer-almagribi <monzeralmagribi@gmail.com>
2021-01-11 10:57:36 +0200

    ___

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -8,6 +8,10 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -17,6 +21,10 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digit=(6, 2))
     seats = fields.Integer(string="Number Of Seats")
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
 #
 #     @api.depends('value')
 #     def _value_pc(self):
